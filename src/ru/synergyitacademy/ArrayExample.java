40dc400369e0a2455a81197846fd0da44512395c
ViTr <ViTr@list.ru>
2020-05-31 21:41:55 +0400

    feat: Add Fibonnachi logic

--- src/ru/synergyitacademy/ArrayExample.java
+++ src/ru/synergyitacademy/ArrayExample.java
@@ -1,6 +1,6 @@
 package ru.synergyitacademy;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayExample {
 
@@ -13,10 +13,21 @@ public class ArrayExample {
         for (int i = 3; i < fibbonaciArray.length; i++) {
             fibbonaciArray[i] = fibonachi(i, fibbonaciArray);
         }
-        System.out.println(Arrays.toString(fibbonaciArray));
+//        System.out.println(Arrays.toString(fibbonaciArray));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter index of Fibbonachi array:");
+        while (scanner.hasNext()) {
+            String value = scanner.nextLine();
+            if ("exit".equals(value)) {
+                break;
+            }
+            Integer index = Integer.decode(value);
+            System.out.printf("%d index of Fibbonachi array %s", index, fibbonaciArray[index -1]);
+        }
     }
 
     public static int fibonachi(int index, int[] fibbonachiArray) {
-        return fibbonachiArray[index - 2] + fibbonachiArray[index - 3];
+        return fibbonachiArray[index - 1] + fibbonachiArray[index - 2];
     }
 }
