fcb56ece0735230afe40eb1d65611e696c95c3b6
ViTr <ViTr@list.ru>
2020-06-03 20:25:54 +0400

    feat: Start work with hometask #4

--- src/ru/synergyitacademy/ArrayExample.java
+++ src/ru/synergyitacademy/ArrayExample.java
@@ -6,13 +6,11 @@ public class ArrayExample {
 
     public static void main(String[] args) {
 //        int [] fibbonaciArray;
-        int fibbonaciArray[] = new int[30];
+        Integer fibbonaciArray[] = new Integer[256];
         fibbonaciArray[0] = 1;
         fibbonaciArray[1] = 1;
         fibbonaciArray[2] = 2;
-        for (int i = 3; i < fibbonaciArray.length; i++) {
-            fibbonaciArray[i] = fibonachi(i, fibbonaciArray);
-        }
+
 //        System.out.println(Arrays.toString(fibbonaciArray));
 
         Scanner scanner = new Scanner(System.in);
@@ -23,11 +21,23 @@ public class ArrayExample {
                 break;
             }
             Integer index = Integer.decode(value);
-            System.out.printf("%d index of Fibbonachi array %s", index, fibbonaciArray[index -1]);
+            if (fibbonaciArray[index - 1] == null ||
+                    fibbonaciArray[index - 1] == 0) {
+                System.out.println("Value has been calculated");
+                for (int i = 3; i < index; i++) {
+                    fibbonaciArray[i] = fibonachi(i, fibbonaciArray);
+                }
+            }
+            else {
+                System.out.println("Value has got from cache");
+            }
+            int calculatedFibbonachiValue = fibbonaciArray[index -1];
+            System.out.printf("%d index of Fibbonachi array %s\n", index, calculatedFibbonachiValue);
+            System.out.println("Enter index of Fibbonachi array:");
         }
     }
 
-    public static int fibonachi(int index, int[] fibbonachiArray) {
+    public static int fibonachi(int index, Integer[] fibbonachiArray) {
         return fibbonachiArray[index - 1] + fibbonachiArray[index - 2];
     }
 }
